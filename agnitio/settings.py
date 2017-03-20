8df8bfd5d1dea21308d4ae1edbd389bf14a127eb
bujordet <mo.bujordet@gmail.com>
2017-03-20 15:43:33 +0100

    initial commit

--- agnitio/settings.py
+++ agnitio/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'assignments'
 ]
 
@@ -131,5 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'assignments'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
