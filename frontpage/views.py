a3df44ace0771793c71986bed6c4fbfc6ac1dfe7
bujordet <mo.bujordet@gmail.com>
2017-03-22 13:17:09 +0100

    Now it is posible to see what subject the assignment is all about

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-
+from assignments.models import *
 # Create your views here.
 
 def index(request):
-
-    return render(request, 'frontpage/frontpage.html')
-
-def subject_list(request):
     subjects = Subject.objects.all()
-    return render(request, 'frontpage/active.html', {
-        'subjects' : subjects
+    return render(request, 'frontpage/frontpage.html', {
+        'subjects': subjects
     })
