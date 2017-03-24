86e35278fbbddba113b96f1c3a16dbc15e0b8468
Dag Erik Gjørvad <Dag Erik Gjrvad>
2017-03-24 14:29:04 +0100

    Oppdattert resultatuthentingen

--- frontpage/views.py
+++ frontpage/views.py
@@ -3,7 +3,13 @@ from assignments.models import *
 # Create your views here.
 
 def index(request):
+
+    user = request.user
+    perms = user.get_all_permissions(obj=None)
     subjects = Subject.objects.all()
-    return render(request, 'frontpage/frontpage.html', {
-        'subjects': subjects
+    if 'professorperms' in perms:
+        return render(request,'assignments/new.html')
+    else:
+        return render(request, 'frontpage/frontpage.html', {
+            'subjects': subjects
     })
